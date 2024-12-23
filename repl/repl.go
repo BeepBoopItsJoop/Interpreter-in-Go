db5406d447e8aad1bc8e0c91e81beb5d15e8252e
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2024-12-23 08:24:52 +0200

    Added evaluation of objects; Boolean literals, Prefix and Infix expressions are now supported

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -29,13 +30,16 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
-const MONKEY_FACE = `            __,__
+const MONKEY_FACE = `
+            __,__
    .--.  .-"     "-.  .--.
   / .. \/  .-. .-.  \/ .. \
  | |  '|  /   Y   \  |'  | |
