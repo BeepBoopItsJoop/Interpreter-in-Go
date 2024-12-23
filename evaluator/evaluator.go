e99f90976d06859c80aeada088f87b43f59cad49
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2024-12-23 11:34:00 +0200

    Support for Return statements added

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,16 +16,20 @@ func Eval(node ast.Node) object.Object {
 
 	// Statements
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
-	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
-
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
+
+	case *ast.BlockStatement:
+		return evalBlockStatement(node)
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -54,11 +58,29 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	return FALSE
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJECT {
+			return result
+		}
 	}
 
 	return result
